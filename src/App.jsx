742c88635fc9d07d701e354cee6f96e8bf1da411
Ana Larissa Mendes <152804250+LissaaMM@users.noreply.github.com>
2025-05-29 10:25:11 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import React from 'react';
-import ListaTarefas from './components/ListaTarefas';
-
-const App = () => {
-  return (
-    <div>
-      <img src='../src/img/foto.png'></img>
-      <h1>Minha Lista de Tarefas</h1>
-      <ListaTarefas />
-    </div>
-  );
-};
-
-export default App;
+import ListaTarefas from "./components/ListaTarefas"
+import './estilo.css'
+export default function App(){
+  return(
+      <div className="body">
+        <ListaTarefas/>
+      </div>
+    )
+}
